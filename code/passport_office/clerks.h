a380b9bce88e428339544d2a6ccba61c01794cec
Matthew Burke <matthelb@usc.edu>
2015-09-20 18:11:08 -0700

    Make Clerk destructors virtual

--- code/passport_office/clerks.h
+++ code/passport_office/clerks.h
@@ -11,7 +11,7 @@ class PassportOffice;
 class Clerk {
  public:
  	Clerk(PassportOffice* passport_office, int identifier);
- 	~Clerk();
+ 	virtual ~Clerk();
  	int CollectMoney();
  	void Run();
 
@@ -41,24 +41,28 @@ class Clerk {
 class ApplicationClerk : public Clerk {
  public:
  	ApplicationClerk(PassportOffice* passport_office, int identifier);
+	virtual ~ApplicationClerk() { }
  	void ClerkWork();
 };
 
 class PictureClerk : public Clerk {
  public:
  	PictureClerk(PassportOffice* passport_office, int identifier);
+	virtual ~PictureClerk() { }
  	void ClerkWork();
 };
 
 class PassportClerk : public Clerk {
  public:
  	PassportClerk(PassportOffice* passport_office, int identifier);
+	virtual ~PassportClerk() { }
  	void ClerkWork();
 };
 
 class CashierClerk : public Clerk {
  public:
  	CashierClerk(PassportOffice* passport_office, int identifier);
+	virtual ~CashierClerk() { }
  	void ClerkWork();
 };
 
